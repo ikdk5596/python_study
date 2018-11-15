f9b7503157c6fb3841acb583c3ddacbebece658c
jun_il <41735011+junilshin@users.noreply.github.com>
2018-11-15 22:40:43 +0900

    Add files via upload

---
+++ thisweek/11-15/junil_11-15.py
@@ -0,0 +1,16 @@
+
+#11-15 목
+symbols = '&$@!!'
+codes = []
+
+for symbol in symbols:
+    codes.append(ord(symbol))
+
+print(codes)
+
+symbols1 = '&$@!!'
+codes1 = [ord(symbol) for symbol in symbols1]
+
+print(codes1)
+
+
